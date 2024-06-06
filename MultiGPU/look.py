142486c1ecf743401bfd23e7bf5c737d2ceac04a
Alexander-CHINALee <17888827263@163.com>
2024-06-06 13:56:09 +0800

    v3

--- MultiGPU/look.py
+++ MultiGPU/look.py
@@ -1,4 +1,8 @@
 import torch
 
-t1 =torch.tensor(2,16,10)
-t2 = torch.tensor(2,4,10)
+t1 =torch.ones(2,16,10)
+t2 = torch.zeros(2,4,10)
+print(t1.shape)
+print(t2.shape)
+t3 = torch.cat((t1,t2),dim=1)
+print(t3.shape)
